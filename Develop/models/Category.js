bb79eee2895d8832988bd53bf4ca2715800a798a
Matthew_Hill <hillmatt58@gmail.com>
2024-02-18 20:07:06 -0700

    Models created

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    category_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
